86b2c378370ae9a7a4995ab212504688c0e52de7
yue <42475@qq.com>
2020-04-05 10:43:28 +0800

    elementui整合

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+// 导入elementui 及其 样式
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+// 注册
+Vue.use(ElementUI);
 
 Vue.config.productionTip = false
 
